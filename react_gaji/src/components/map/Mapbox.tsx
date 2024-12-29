171dd0aa49d52b818f6e4ed8f81a4ea853e3dab2
RedbeenStealer <ttuleula@gmail.com>
2024-12-29 18:35:17 +0900

    맵박스 컴포넌트 -> Mainpage 임포트
    기본 맵박스 컴포넌트를 Mainpage에 임포트
    이후 작업목록: 현재 유저위치 불러오기, 위치기반 맵마커 찍기

--- react_gaji/src/components/map/Mapbox.tsx
+++ react_gaji/src/components/map/Mapbox.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
+import useMap from "../../hooks/map/useMap";
+import { mapConfig } from "../../config/mapConfig";
 
 const Mapbox:React.FC = () => {
+    const mapContainer = useRef<HTMLDivElement>(null);
+
+    useMap({
+        mapContainerRef: mapContainer,
+        style: mapConfig.defaultStyle,
+        config: mapConfig
+    });
+
     return (
-        <div></div>
-    )
+        <div 
+            ref={mapContainer}
+            style={{ width: '100%', height: '100vh'}}
+        />
+    );
 }
 
 export default Mapbox;
