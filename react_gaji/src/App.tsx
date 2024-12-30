a94911db86bbf52f2f3107eecb0adad50a0db92e
qingzhi88 <wonew123@naver.com>
2024-12-30 14:41:10 +0900

    modified:   react_gaji/src/App.tsx

--- react_gaji/src/App.tsx
+++ react_gaji/src/App.tsx
@@ -8,12 +8,13 @@ import Productlistpage from "./pages/Productlistpage";
 import Productpage from "./pages/Productpage";
 import Signup from "./pages/Signup";
 import Mypage from "./pages/Mypage";
+import MypageProfileEdit from './pages/Mypage_profileedit';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Mainpage/>} />
         <Route path="/chatlist" element={<Chatlist/>} />
@@ -24,6 +25,7 @@ function App() {
         <Route path="/productpage" element={<Productpage/>} />
         <Route path="/signup" element={<Signup/>} />
         <Route path="/mypage" element={<Mypage/>} />
+        <Route path="/mypage_profileedit" element={<MypageProfileEdit/>} />
       </Routes>
     </BrowserRouter>
     </>
