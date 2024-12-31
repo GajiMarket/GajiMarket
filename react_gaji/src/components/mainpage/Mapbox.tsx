492ee95bb8b5549916c71326cdb18f9334d637f3
112park <psh676@naver.com>
2024-12-31 16:40:04 +0900

    my_location_241231
    내위치 찍기

--- react_gaji/src/components/mainpage/Mapbox.tsx
+++ react_gaji/src/components/mainpage/Mapbox.tsx
@@ -2,17 +2,39 @@ import React, { useRef } from "react";
 import useMap from "../../hooks/useMap";
 import { mapConfig } from "../../config/mapConfig";
 import "../../style/Mapbox.css";
+import gps_icon from "../../img/gps_icon.png";
 
-const Mapbox:React.FC = () => {
+const Mapbox: React.FC = () => {
     const mapContainerRef = useRef<HTMLDivElement>(null);
-    useMap(mapContainerRef, mapConfig.defaultStyle, mapConfig);
+    const { updateLocation } = useMap(mapContainerRef, mapConfig.defaultStyle, mapConfig);
+
+    // 내 위치 버튼 클릭 핸들러
+    const handleMyLocation = () => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.watchPosition(
+                (position) => {
+                    const { latitude, longitude } = position.coords;
+                    updateLocation(longitude, latitude);
+                },
+                (error) => {
+                    console.error("Error fetching location:", error.message);
+                    alert("위치를 가져올 수 없습니다.");
+                },
+                { enableHighAccuracy: true }
+            );
+        } else {
+            alert("브라우저에서 위치 정보를 지원하지 않습니다.");
+        }
+    };
 
     return (
-        <div 
-            ref={mapContainerRef}
-            className="Mapbox_Googlemap"
-        />
+        <>
+            <div ref={mapContainerRef} className="Mapbox_Googlemap" />
+            <button onClick={handleMyLocation} className="Mapbox_Googlemap_myLocation">
+                <img src={gps_icon} className="Mapbox_Googlemap_gps_icon" />
+            </button>
+        </>
     );
-}
+};
 
 export default Mapbox;
