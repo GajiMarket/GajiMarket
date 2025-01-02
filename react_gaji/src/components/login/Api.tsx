5bc396182039439bc3829e4f75cdeda154d13095
LeeAnnHyung <hyung9683@gmail.com>
2025-01-02 18:21:48 +0900

    로그인 폼 거의 다만들어가는중

--- react_gaji/src/components/login/Api.tsx
+++ react_gaji/src/components/login/Api.tsx
@@ -12,13 +12,19 @@ const Api:React.FC<IApiProps> = ({kakaoApi, naverApi, googleApi}) => {
   return (
     <div className="login_api">
       <div className="kakao">
-        <button type="button" className="kakao_button" onClick={kakaoApi}></button>
+        <button type="button" className="kakao_button" onClick={googleApi}>
+          <img src="../../public/img/google_button.png" />
+        </button>
       </div>
       <div className="naver">
-        <button type="button" className="naver_button" onClick={naverApi}></button>
+        <button type="button" className="naver_button" onClick={kakaoApi}>
+         <img src="../../public/img/Kakao_button.png" />
+        </button>
       </div>
       <div className="google">
-        <button type="button" className="google_button" onClick={googleApi}></button>
+        <button type="button" className="google_button" onClick={naverApi}>
+          <img src="../../public/img/Kakao_button.png" />
+        </button>
       </div>
     </div>
   )
