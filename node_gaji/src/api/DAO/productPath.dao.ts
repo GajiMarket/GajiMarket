07871f55a6184513320900b0f1c4db21cd797e96
RBS <153577632+RebbeenStealer@users.noreply.github.com>
2025-01-06 19:36:29 +0900

    DB 연결 테스트

--- node_gaji/src/api/DAO/productPath.dao.ts
+++
@@ -1,8 +0,0 @@
-import { db, schema } from '../../config/dbConfig';
-import { IProduct } from '../models/product'
-
-export const getProducts = async (lng: number, lat: number, distance: number = 500): Promise<IProduct[]> => {
-    const response = `
-        SELECT * FROM ${schema}.product AS 
-    `
-};
---
+++ react_gaji/src/components/mainpage/PopupContent.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+import axios from "axios";
+
+const PopupContent: React.FC = () => {
+    const productContent = 
+}
+
+export default PopupContent;
