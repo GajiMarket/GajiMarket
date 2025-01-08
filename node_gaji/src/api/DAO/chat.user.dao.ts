c57a5ab3dee20a1e1befdd07191c920f2a1a9281
qingzhi88 <wonew123@naver.com>
2025-01-08 16:10:36 +0900

    채팅 API 작업

--- node_gaji/src/api/DAO/chat.user.dao.ts
+++ node_gaji/src/api/DAO/chat.user.dao.ts
@@ -1,2 +1,7 @@
 import { db, schema } from '../../config/dbConfig';
-import { IChatUser } from '../models/chat_user.models';
+import { IChatUser } from '../models/chat.user.models';
+
+export const getChatUserById = async (id: number): Promise<IChatUser | null> => {
+    const result = await db.query('SELECT * FROM member_tbl WHERE member_no = $1', [id]);
+    return result.rows[0] || null;
+}
