1b9e54eed3fdf6c43474000afa6b7c7b47acf903
Najieun6 <najiji71@gmail.com>
2025-01-09 12:19:40 +0900

    처음 화면 로그인 화면으로 하기

--- react_gaji/src/App.tsx
+++ react_gaji/src/App.tsx
@@ -20,59 +20,53 @@ import MypageNeighborhoodAuth from "./components/mypage/Mypage_neighborhood_auth
 import MypageKeywordSettings from "./components/mypage/Mypage_keyword_settings";
 import MypageAlarm from "./components/mypage/Mypage_alarm"; // 알림 페이지 추가
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Navigation from "./pages/Navigation";
-import { FooterProvider } from "./components/all/FooterContext";
+import Navigation from './pages/Navigation';
+import { FooterProvider } from './components/all/FooterContext';
 import Lodingpage from "./pages/Lodingpage";
-import FindMain from "./pages/FindMain";
+import FindMain from './pages/FindMain';
 import ZustandTest from "./pages/ZustandTest";
 
 const App: React.FC = () => {
   return (
-    <FooterProvider>
-      <BrowserRouter>
-        <>
-          <Routes>
-            <Route path="/" element={<Mainpage />} />
-            <Route path="/lodingpage" element={<Lodingpage />} />
-            <Route path="/chatlist" element={<Chatlist />} />
-            <Route path="/chatpage/:id" element={<Chatpage />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/find" element={<FindMain />} />
-            <Route path="/productadd" element={<Productadd />} />
-            <Route path="/productlistpage" element={<Productlistpage />} />
-            <Route path="/productpage" element={<Productpage />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/mypage" element={<Mypage />} />
-            <Route path="/mypage_profileedit" element={<MypageProfileEdit />} />
-            <Route path="/mypage_love" element={<MypageLove />} />
-            <Route path="/mypage_sell" element={<MypageSell />} />
-            <Route path="/mypage_buy" element={<MypageBuy />} />
-            <Route path="/mypage_faq" element={<MypageFAQ />} />
-            <Route path="/mypage_terms" element={<MypageTerms />} />
-            <Route
-              path="/mypage_terms_detail"
-              element={<MypageTermsDetail />}
-            />
-            <Route
-              path="/mypage_neighborhood_settings"
-              element={<MypageNeighborhoodSettings />}
-            />
-            <Route
-              path="/mypage_neighborhood_auth"
-              element={<MypageNeighborhoodAuth />}
-            />
-            <Route
-              path="/mypage_keyword_settings"
-              element={<MypageKeywordSettings />}
-            />
-            <Route path="/mypage_alarm" element={<MypageAlarm />} />{" "}
-            {/* 알림 페이지 추가 */}
-            <Route path="/navigation" element={<Navigation />} />
-            <Route path="/zustandtest" element={<ZustandTest />} />
-          </Routes>
-        </>
-      </BrowserRouter>
-    </FooterProvider>
+      <FooterProvider>
+        <BrowserRouter>
+            <Routes>
+              <Route path="/map" element={<Mainpage />} />
+              <Route path="/lodingpage" element={<Lodingpage/>} />
+              <Route path="/chatlist" element={<Chatlist />} />
+              <Route path="/chatpage/:id" element={<Chatpage />} />
+              <Route path="/" element={<Login />} />
+              <Route path="/find" element={<FindMain />} />
+              <Route path="/productadd" element={<Productadd />} />
+              <Route path="/productlistpage" element={<Productlistpage />} />
+              <Route path="/productpage" element={<Productpage />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/mypage" element={<Mypage />} />
+              <Route path="/mypage_profileedit" element={<MypageProfileEdit />} />
+              <Route path="/mypage_love" element={<MypageLove />} />
+              <Route path="/mypage_sell" element={<MypageSell />} />
+              <Route path="/mypage_buy" element={<MypageBuy />} />
+              <Route path="/mypage_faq" element={<MypageFAQ />} />
+              <Route path="/mypage_terms" element={<MypageTerms />} />
+              <Route path="/mypage_terms_detail" element={<MypageTermsDetail />} />
+              <Route
+                path="/mypage_neighborhood_settings"
+                element={<MypageNeighborhoodSettings />}
+              />
+              <Route
+                path="/mypage_neighborhood_auth"
+                element={<MypageNeighborhoodAuth />}
+              />
+              <Route
+                path="/mypage_keyword_settings"
+                element={<MypageKeywordSettings />}
+              />
+              <Route path="/mypage_alarm" element={<MypageAlarm />} /> {/* 알림 페이지 추가 */}
+              <Route path="/navigation" element={<Navigation />} />
+              <Route path="/zustandtest" element={<ZustandTest />} />
+            </Routes>
+        </BrowserRouter>
+      </FooterProvider>
   );
 };
 
