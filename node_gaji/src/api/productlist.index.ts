dd8ab76cef3d34e846737588d54045b86790a856
Najieun6 <najiji71@gmail.com>
2025-01-14 11:35:18 +0900

    produclist index는 product가 있어서 필요 없어서 삭제

--- node_gaji/src/api/productlist.index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getProductListCtrl } from "./controller/productlist.ctrl"; // 컨트롤러 가져오기
-
-const router = express.Router();
-
-// 제품 목록 가져오기 라우터
-router.get("/product/list", getProductListCtrl);
-
-export default router;
