9aa87872a8f1561ba78fdd8ebe9fb57c38b1a801
qingzhi88 <wonew123@naver.com>
2025-01-14 16:03:18 +0900

    modified:   node_gaji/src/api/pathFinder.index.ts 	modified:   node_gaji/src/api/routes.ts

--- node_gaji/src/api/pathFinder.index.ts
+++ node_gaji/src/api/pathFinder.index.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import { productPathCtrl } from "./controller/productPath.ctrl";
-import { pathFinderAPI } from "./service/productPath.service";
+// import { pathFinderAPI } from "./service/productPath.service";
 
 const router = express.Router();
 
 router.post('/', productPathCtrl)
-router.get('/', pathFinderAPI)
+// router.get('/', pathFinderAPI)
 
 export default router
--- node_gaji/src/api/routes.ts
+++ node_gaji/src/api/routes.ts
@@ -6,21 +6,21 @@ import { userLocation } from './controller/userLocation.ctrl';
 import member from './member.index';
 import chatRoutes from './chat.index';
 import mypage from '../api/mypage.index';
-import productListRoutes from '../api/productlist.index';
+// import productListRoutes from '../api/productlist.index';
 // import productcdd from './productcdd.index';
 
-const mountRoutes = (app:Express) => {
+const mountRoutes = (app: Express) => {
 
     app.use('/auth', member);
 
     app.use('/map', Product_preview)
 
     app.use('/product', products)
-    
-//     app.use('/productadd', productcdd )
-    
+
+    // app.use('/productadd', productcdd )
+
     app.use('/navigation', path)
-    
+
     app.post('/', userLocation)
 
     app.use('/api', chatRoutes);
