1a2658c17bc1f878562c534c8d0a7fa3bbc2b2e7
qingzhi88 <wonew123@naver.com>
2025-01-15 17:38:14 +0900

    modified:   node_gaji/src/api/DAO/member.auth.dao.ts 	deleted:    node_gaji/src/api/controller/product.ctrl.ts 	modified:   node_gaji/src/api/controller/productadd.ctrl.ts 	modified:   node_gaji/src/api/product.index.ts

--- node_gaji/src/api/controller/productadd.ctrl.ts
+++ node_gaji/src/api/controller/productadd.ctrl.ts
@@ -1,15 +1,15 @@
 import { Request, Response } from "express";
 import { addfinderAPI } from "../service/productadd.service";
-import logger from "../../logger";
+import { logger } from "../../logger";
 // import { IProduct } from "api/models/product";
 
 export const productaddCtrl = async (req: Request, res: Response): Promise<void> => {
-    
-    
-    try {
-        const { title, sell_price, description, location, userNo, status } = req.body;
-        logger.info("Received product data:", req.body);
-        console.log("req.body data :",req.body)
+
+
+  try {
+    const { title, sell_price, description, location, userNo, status } = req.body;
+    logger.info("Received product data:", req.body);
+    console.log("req.body data :", req.body)
 
     if (!title || !sell_price || !description || !location || !userNo || !status) {
       logger.error("Missing required fields");
@@ -20,28 +20,28 @@ export const productaddCtrl = async (req: Request, res: Response): Promise<void>
       return;
     }
     const result = await addfinderAPI({
-        title,
-        sell_price,
-        description,
-        location,
-        userNo,
-        status,
-        created_at: new Date(),
-        view_count: 0,
-      });
+      title,
+      sell_price,
+      description,
+      location,
+      userNo,
+      status,
+      created_at: new Date(),
+      view_count: 0,
+    });
 
 
-        res.status(200).json({
-            success: true,
-            message: "Product added successfully",
-            data: result,
-        });
-    } catch (error) {
-        console.error("Error productadd request:", error);
-        res.status(500).json({
-            success: false,
-            message: "Internal server error.",
-            details: error instanceof Error ? error.stack : "Unknown error",
-        });
-    }
+    res.status(200).json({
+      success: true,
+      message: "Product added successfully",
+      data: result,
+    });
+  } catch (error) {
+    console.error("Error productadd request:", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error.",
+      details: error instanceof Error ? error.stack : "Unknown error",
+    });
+  }
 }
--- node_gaji/src/api/product.index.ts
+++ node_gaji/src/api/product.index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import { getProductListCtrl } from './controller/productlist.ctrl';
-import { addProductCtrl } from './controller/product.ctrl';
 // import { productDetail } from "./controller/productDetail.ctrl";
 
 const router = express.Router();
@@ -8,7 +7,6 @@ const router = express.Router();
 
 router.get('/list', getProductListCtrl)
 
-router.post('/', addProductCtrl);
 
 // router.get('/detail', productDetail);
 
