f774589838e115f7a71a152ddc038d482a00d788
YukyungShim <mancy99@naver.com>
2025-01-16 18:15:53 +0900

    livelivestarstarseesee

--- node_gaji/src/api/controller/mypage_love.ctrl.ts
+++ node_gaji/src/api/controller/mypage_love.ctrl.ts
@@ -1,29 +1,28 @@
 import { Request, Response } from "express";
-import { wishlistService } from "../service/wishlist.service";
+import { mypageLoveService } from "../service/mypage_love.service";
 
 // 관심목록 가져오기
-export const getWishlist = async (req: Request, res: Response) => {
+export const getMypageLove = async (req: Request, res: Response) => {
     const member_no = Number(req.params.member_no);
-
     try {
-        const wishlist = await wishlistService.fetchWishlist(member_no);
-        res.status(200).json(wishlist);
+        const mypageLove = await mypageLoveService.fetchMypageLove(member_no);
+        res.status(200).json(mypageLove);
     } catch (error) {
-        console.error("Failed to fetch wishlist:", error);
-        res.status(500).json({ error: "Failed to fetch wishlist" });
+        console.error("Failed to fetch Mypage Love:", error);
+        res.status(500).json({ error: "Failed to fetch Mypage Love" });
     }
 };
 
-// 관심목록에서 항목 삭제
-export const deleteWishlistItem = async (req: Request, res: Response) => {
-    const member_no = Number(req.params.member_no);
+// 관심목록 항목 삭제
+export const deleteMypageLoveItem = async (req: Request, res: Response) => {
     const product_id = Number(req.params.product_id);
-
+    const member_no = Number(req.params.member_no);
     try {
-        await wishlistService.deleteWishlistItem(product_id, member_no);
-        res.status(200).json({ message: "Item removed from wishlist" });
+        await mypageLoveService.deleteMypageLoveItem(product_id, member_no);
+        res.status(200).json({ message: "Item removed from Mypage Love" });
     } catch (error) {
-        console.error("Failed to delete wishlist item:", error);
-        res.status(500).json({ error: "Failed to delete wishlist item" });
+        console.error("Failed to delete Mypage Love item:", error);
+        res.status(500).json({ error: "Failed to delete Mypage Love item" });
     }
 };
+
