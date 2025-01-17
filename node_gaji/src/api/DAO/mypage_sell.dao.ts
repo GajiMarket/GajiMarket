3eece27173f0c8ceaae5b6b8df15285a60a06838
YukyungShim <mancy99@naver.com>
2025-01-17 18:14:21 +0900

    Query_change

--- node_gaji/src/api/DAO/mypage_sell.dao.ts
+++ node_gaji/src/api/DAO/mypage_sell.dao.ts
@@ -3,27 +3,26 @@ import { db } from "../../config/dbConfig";
 // 판매내역 가져오기
 export const getSellHistory = async (member_no: number) => {
     const query = `
-        SELECT 
-            p.product_id,
-            p.title,
-            p.sell_price,
-            p.status,
-            p.created_at,
-            ph.image AS product_image
-        FROM 
-            team4.product p
-        LEFT JOIN 
-            team4.photo ph
-        ON 
-            p.product_id = ph.product_id
-        WHERE 
-            p.member_no = $1
-        ORDER BY 
-            p.created_at DESC
+      SELECT 
+        p.product_id,
+        p.title,
+        p.sell_price,
+        p.location,
+        p.created_at,
+        p.status,
+        ph.image_url AS image
+      FROM 
+        team4.product AS p
+      LEFT JOIN 
+        team4.photo AS ph ON p.product_id = ph.product_id
+      WHERE 
+        p.seller_id = $1
     `;
-    const result = await db.query(query, [member_no]);
+    const values = [member_no];
+    const result = await db.query(query, values);
     return result.rows;
-};
+  };
+  
 
 // 판매 상태 업데이트
 export const updateSellStatus = async (product_id: number, status: string) => {
