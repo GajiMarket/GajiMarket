79a3496bfe9e94affc8ba6fc507d71f2134a5116
LeeAnnHyung <hyung9683@gmail.com>
2025-01-18 17:29:53 +0900

    지도 마커 완성

--- react_gaji/src/components/map/ProductDetailMap.tsx
+++ react_gaji/src/components/map/ProductDetailMap.tsx
@@ -19,11 +19,11 @@ type Location = Partial<LocationProps>
 const ProductDetailMap:React.FC<Location> = ({locationData}) => {
 
 
-  const { userMarker, setUserMarker, userLocation, setUserLocation } = productStroe();
+  // const { userMarker, setUserMarker, userLocation, setUserLocation } = productStroe();
   // const {productLocations, fetchProductLocations, renderMarkers} = useMarkers();
 
     const mapContainerRef = useRef<HTMLDivElement>(null);
-    const { mapInstance, updateCenter, parsepoint} = detailMap(mapContainerRef, mapConfig);
+    const { mapInstance, updateCenter, parsePoint} = detailMap(mapContainerRef, mapConfig);
    
 
     const createCustomMarker = (): HTMLElement => {
@@ -33,71 +33,24 @@ const ProductDetailMap:React.FC<Location> = ({locationData}) => {
 
     };
 
-    updateCenter(mapConfig.initialCenter[0], mapConfig.initialCenter[1]);
+useEffect(() => {
 
-  
-   
-
-    const parsePoint = (point: string): [number, number] | null => {
-
-      try {
-        // "POINT("를 ""로 대체하고 ")"를 ""로 대체해서 " "을 기준으로 분리한다다
-        const coordinates = point.replace("POINT(", "").replace(")", "").split(" ");
-        const lng = parseFloat(coordinates[0]); // 경도
-        const lat = parseFloat(coordinates[1]); // 위도
-
-        console.log("lng와 lat 값",lng, lat);
-        
-        
-        return [lng, lat];
-
-      } catch (error) {
-
-        console.error("POINT를 가져오지 못했습니다:", error);
-        return null;
-        
-      }
-    };
-
+  if(mapInstance) {
     
-    const coordinates = parsePoint(locationData as string) as [number, number];
 
-    console.log("현재 coordinates:", coordinates);
+    // updateCenter(mapConfig.initialCenter[0], mapConfig.initialCenter[1]);
 
-    const [lng, lat] = coordinates;
+    const newLocation = parsePoint(locationData as string);
 
-    setUserLocation?.([lng, lat]);
-
-    console.log("현재 userLocation:", userLocation);
-    
-    useEffect(() => {
-      if(!locationData) {
-        console.error("locationData가 없습니다.");
-        return;
-      }
+    updateCenter(newLocation?.[0] as number, newLocation?.[1] as number);
 
 
-      const coordinates = parsePoint(locationData);
 
-      if(!coordinates) {
 
-        console.error("좌표를 파싱하지 못했습니다.");
+  }
 
-        return;
-        
-      }
+},[mapInstance, locationData]);
 
-      setUserLocation?.(coordinates);
-
-      if(!mapInstance) {
-        console.error("Mapbox 인스턴스가 없습니다.");
-        return;
-      }
-
-      new mapboxgl.Marker({element: createCustomMarker()}).setLngLat(coordinates).addTo(mapInstance);
-
-      updateCenter(coordinates[0], coordinates[1]);
-    }, [locationData, mapInstance, setUserLocation])
    
 
 
