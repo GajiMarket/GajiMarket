6e3a5243438ff4a71e125656d9bc1fb50d051aa6
LeeAnnHyung <hyung9683@gmail.com>
2025-01-20 01:33:51 +0900

    productadd 이미지 업로드 추가중

--- node_gaji/src/api/service/productadd.service.ts
+++ node_gaji/src/api/service/productadd.service.ts
@@ -1,8 +1,11 @@
 import { addProductDAO } from "../DAO/product.dao";
 import { IProduct } from "../models/product";
+import IPhoto from '../models/photo';
 import {logger} from "../../logger";
 
-export const addfinderAPI = async (productData: IProduct) => {
+type Product = Partial<IProduct|IPhoto>
+
+export const addfinderAPI = async (productData: Product) => {
   try {
     logger.info(`Processing product data in service:${productData}`);
     console.log(`productData :${productData}`)
@@ -19,3 +22,21 @@ export const addfinderAPI = async (productData: IProduct) => {
     throw error;
   }
 };
+
+// export const addfinderAPI = async (productData: Product) => {
+//   try {
+//     logger.info(`Processing product data in service:${productData}`);
+//     console.log(`productData :${productData}`)
+//     const result = await addProductDAO(productData);
+//     if (!result) {
+//       logger.error("Product insertion failed in DAO");
+//       throw new Error("Product insertion failed");
+//     }
+
+//     logger.info("Product added successfully in service");
+//     return result;
+//   } catch (error) {
+//     logger.error(`Error in addProductService:${error}`);
+//     throw error;
+//   }
+// };
